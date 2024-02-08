a51d09510505a38c4e1e6a45fdd8fa618053c4a5
Anton Eliseev <anton_saintp@mail.ru>
2024-02-08 12:46:33 +0300

    profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Получение постов пользователя
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = async () => {};
+  const handleDelete = async () => {};
+
   return (
-    <div>page</div>
-  )
-}
+    <Profile
+      name="My"
+      desc="Welcome to your profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
 
-export default page
+export default MyProfile;
